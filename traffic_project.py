b90c2abfb5a194bcb8aa9482c0613a5c4e39a635
swarajsah143 <swarajsah143@gmail.com>
2025-06-13 14:02:48 +0545

    new projects added

---
+++ traffic_project.py
@@ -0,0 +1,22 @@
+color=input("Enter the color shown in traffic light: ")
+if(color=="red"):
+    print("You need to stop!")
+elif(color=="green"):
+    print("You can go!")
+elif(color=="yellow"):
+    print("Be ready to go!")    
+else:
+    print("Traffic light is broken")    
+
+
+
+# age=int(input("Enter the age of the voter: "))
+# result=("no_he canot","yes_he can") [age>=18]   # this is clever if/ Ternary operator
+# print(result)
+
+# a=11
+# b=100
+# print (not(a>b))
+
+# for i in range(1,11,2):     # for i in range(initial, final-1, step)
+#     print(i)
